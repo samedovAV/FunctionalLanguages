d102c275d3d649c39bcd8642976c924e8af82f21
samedovAV <alekcandr206@gmail.com>
2023-03-08 21:52:15 +0300

    hello-world

---
+++ first_functions.hs
@@ -0,0 +1,14 @@
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+    then x
+    else x*2
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUpperCase st = [c | c <- st, c `elem` ['A'..'Z']]
+
